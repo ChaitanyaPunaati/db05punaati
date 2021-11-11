d317ed918f8391af3ea7f6d446fa8af1afda88ba
Chaitanya Punaati <89426302+ChaitanyaPunaati@users.noreply.github.com>
2021-11-11 00:26:04 -0600

    Instances added

--- app.js
+++ app.js
@@ -16,6 +16,55 @@ var usersRouter = require('./routes/users');
 var IceCreamRouter = require('./routes/IceCream');
 var addmodsRouter = require('./routes/addmods');
 var selectorRouter = require('./routes/selector');
+var IceCream = require("./models/IceCream");
+var resource = require("./routes/resource");
+
+// We can seed the collection if needed on
+//Get the default connection
+var db = mongoose.connection;
+//Bind connection to error event
+db.on('error', console.error.bind(console, 'MongoDB connectionerror:'));
+db.once("open", function(){
+console.log("Connection to DB succeeded");
+});
+
+
+// We can seed the collection if needed on
+
+async function recreateDB(){
+ // Delete everything
+ await IceCream.deleteMany();
+ let instance1 = new
+IceCream({
+    IceCreamName: "Choclate",
+Price: 5,
+Quantity: 1});
+ instance1.save( function(err,doc) {
+ if(err) return console.error(err);
+ console.log("First object saved")
+ });
+ let instance2 = new IceCream({
+  IceCreamName: "butterscotch",
+      Price: 67,
+      Quantity: 4
+});
+instance2.save(function (err, doc) {
+  if (err) return console.error(err);
+  console.log("Second object saved")
+});
+let instance3 = new IceCream({
+  IceCreamName: "vanila",
+  Price: 6,
+  Quantity: 3
+});
+instance3.save(function (err, doc) {
+  if (err) return console.error(err);
+  console.log("Third object saved")
+});
+}
+let reseed = true;
+if (reseed) { recreateDB();}
+
 
 var app = express();
 
