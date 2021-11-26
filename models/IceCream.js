349fdf69261a695e4858a0e89ad774a6a5bcdcd2
Chaitanya Punaati <89426302+ChaitanyaPunaati@users.noreply.github.com>
2021-11-26 17:32:36 -0600

    min length updated

--- models/IceCream.js
+++ models/IceCream.js
@@ -1,7 +1,10 @@
 const mongoose = require("mongoose")
 const IceCreamSchema = mongoose.Schema({
-    IceCreamName: String,
-    Price: Number,
-    Quantity: Number
+IceCreamName: {
+    type: String,
+    minLength : 3
+},
+Price: Number,
+Quantity: Number
 })
 module.exports = mongoose.model("IceCream", IceCreamSchema)
